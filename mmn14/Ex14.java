0cc7d3850baa46175163058bf38a019178767655
RF <34131940+drrf@users.noreply.github.com>
2018-01-11 21:59:20 +0200

    fix
    some fix update.

--- mmn14/Ex14.java
+++ mmn14/Ex14.java
@@ -1,7 +1,7 @@
 
 /**
  * This class represents a Ex14 for Open university.
- * exercise about complexity and recursion.
+ * exercise about binary search, complexity and recursion.
  *
  * @author Ron F.
  * @version 1.0
@@ -10,40 +10,38 @@
 
 public class Ex14
 {
-    private static int n; // counter of n complexity
-    private static int arrLength; // counter of array
-    private static int metNum; // method number for print method
+    private static int n; // counter of time complexity
 
     /**
-     * Check how many x found in the array
+     * Check how many times "x" found in array of sort number with ascending order;
      * 
-     * @param a[] array of number
-     * @param x the number to serach in the array
-     * @return How many x found in the array
+     * Time complexity: log 2(n) = log(n) = O(log n);
+     * Space complexity: O(1);
+     * 
+     * @param a[] array of sort number with ascending order
+     * @param x number to serach in the array
+     * @return y times "x" found in the array
      * 
      */
 
     public static int count (int[] a, int x){
-        metNum = 1; // num of method
-        arrLength = a.length; // array length
-        int counter = binarySearch(a, x); // binary search with counter return the number of x in the array
-
-        // System.out.print(printStr());  // print out complexity
+        // binary search with counter return the number of x in the array
+        int counter = binarySearch(a, x);
         return counter;
     } // end of count
 
     /**
-     * Check how many switch need to create alternating String of 0 and 1
+     * Check how many switch need to create alternating String of 0 and 1;
+     * 
+     * Time complexity: O(n/2) = O(n);
+     * Space complexity: O(1);
      * 
      * @param s String of 0 and 1 to check alternating.
-     * @return The number of switch need to be alternating String.
+     * @return x switch need to be alternating String.
      * 
      */
 
     public static int alternating (String s){
-        metNum = 2; // num of method for print method
-        arrLength = s.length(); // array length for print method
-        // char zero = '0';  
         int counterZero = 0;
         int counterOne = 0;
 
@@ -64,10 +62,10 @@ public class Ex14
     } // end of alternating
 
     /**
-     * Check if there is way to go from index 0 to the last cell in the array.
+     * Check if there way from index 0 to the last cell in the array.
      * 
      * @param a Array of numbers to check the way through.
-     * @return true if found way to go to the last cell in the array.
+     * @return true if found way to last cell in the array.
      * 
      */
 
@@ -83,26 +81,36 @@ public class Ex14
         /* array b for exit isWayRecursion:
         if (b [0] = 1 return true; b [1] = 1 return false;) */
         int [] b = new int [1];
+        
+        n = 0;
 
         // start to check
-        // boolean answer = moveRight(a, b, 0, 0, 0);
-        // boolean answer = isWay(a, 0);
-
-        return isWay(a, 0, 0);
-
-        /* if (b[0] == 1) // exit recursion
+        boolean answer = moveRight(a, b, 0, 0, 0);
+        // boolean answer = isWay(a, 0, 0);
+        System.out.println(n + " attempts, before out from recursion!");
+        
+        
+        if (b[0] == 1) // exit recursion
         return true; 
         else
-        return false; */
+        return false;
+        
+        // return answer;
+        
     } // end of isWay
-
+    
+    // this method take more time complexity from moveRight method but don't erase the array
     private static boolean isWay (int[] a, int i, int loop) {
-        // start count loop of recursion
+        // counter of time complexity
+        n++;
+        
+        // counter loop of recursion
         loop++;
-
+        
         // limted recursion to never stack on loop more then the length of the array
         if (loop > a.length){ 
             // System.out.println(loop + " trys, out of recursion limit!");
+            // System.out.println(n + " trys, out of recursion limit!");
             return false;
         }
 
@@ -121,7 +129,7 @@ public class Ex14
      */
 
     public static void printPath (int[][] mat) {
-        System.out.print("(0,0)"); // first print no matter what
+        System.out.print("(0,0)"); // first print the current top, anyway
         path (mat, 0, 0, 0);
     } // end of printPath
 
@@ -197,11 +205,13 @@ public class Ex14
 
         int sum = upperCheck-lowerCheck; // calc the sum of how many num show up in the array
         return sum;
-
     } // end of binarySearch
 
     private static boolean moveRight (int [] a, int [] b, int indCell, int indTemp, int loop) {
-        // start count loop of recursion
+        // counter of time complexity
+        n++;
+        
+        // counter loop of recursion
         loop++;
 
         // limted recursion to never stack on loop more then the length of the array
@@ -289,7 +299,7 @@ public class Ex14
         if (line+1 < mat.length){ // try dwon
             if (mat[line][column] < mat[line+1][column]){ // dwon is bigger ?
                 line++;
-                System.out.print("(" + line + "," + column + ")");
+                System.out.print("(" + line + "," + column + ")"); // print
             } else {
                 finshD = true;
             } 
@@ -300,7 +310,7 @@ public class Ex14
         if (column+1 < mat.length){ // try right
             if (mat[line][column] < mat[line][column+1]){ // right is bigger ?
                 column++;
-                System.out.print("(" + line + "," + column + ")");
+                System.out.print("(" + line + "," + column + ")"); // print
             } else {
                 finshR = true;
             }
@@ -311,7 +321,7 @@ public class Ex14
         if (line-1 < mat.length && line-1 >= 0){ // try up
             if (mat[line][column] < mat[line-1][column] ){ // up is bigger ?
                 line--;
-                System.out.print("(" + line + "," + column + ")");
+                System.out.print("(" + line + "," + column + ")"); // print
             } else {
                 finshU = true;
             }
@@ -322,7 +332,7 @@ public class Ex14
         if (column-1 < mat.length && column-1 >= 0){ // try left
             if (mat[line][column] < mat[line][column-1]){ // left is bigger ?
                 column--;
-                System.out.print("(" + line + "," + column + ")");
+                System.out.print("(" + line + "," + column + ")"); // print
             } else {
                 finshL = true;
             }
@@ -338,21 +348,4 @@ public class Ex14
         else 
             return "";
     } // end of path
-
-    private static String printStr (){
-        String s1 = "";
-
-        if (n < arrLength/2 && n > 0 && metNum == 1){
-            System.out.println("WORST CASE: O(log 2(n)*2) = log 2(" + arrLength + ")*2; \n"
-                + "n = " + Ex14.n + " times to find \"num\", from: " + arrLength + " array length;");
-        }
-
-        if (n <= arrLength/2 && n > 0 && metNum == 2){
-            System.out.println("O(n/2); "
-                + "n = " + Ex14.n + " times check string to alternating, from: " + arrLength + " charts.");
-        }
-        arrLength = 0; // reset 
-        n = 0; // reset n counter
-        return s1;
-    } // end of printStr
 } // end of the class
